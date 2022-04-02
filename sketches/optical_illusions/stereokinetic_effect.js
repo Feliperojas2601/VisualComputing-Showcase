1e7ac4986e65e1e757dc806e1ee2deea8063c93d
Jonatlop07 <Jonatlop07@users.noreply.github.com>
2022-04-02 17:53:52 +0000

    deploy: 5c438b1595c4d20f6551813dc2b37b738127f6da

---
+++ sketches/optical_illusions/stereokinetic_effect.js
@@ -0,0 +1,35 @@
+const frame_rate = 60;
+
+function setup() {
+  createCanvas(500, 500);
+  frameRate(frame_rate);
+}
+
+function draw() {
+  background(220);
+  
+  let difference = 40;
+  let inner_diameter = 40;
+  
+  const outer_circles = 11;
+  const start = inner_diameter + outer_circles * difference;
+  const end = inner_diameter;
+  
+  noStroke();
+  
+  let posX = 0, posY = 0;
+  let referenceX = width / 2;
+  let referenceY = height / 2;
+  
+  for (let diameter = start, index = 0; diameter >= end; diameter -= difference, index++) {
+    fill(index % 2 === 0 ? color('blue') : color('yellow'));
+    let orientation = index > 6 ? -1 : 1;
+    posX = referenceX + orientation * index * difference / 2 * cos(frameCount / frame_rate);
+    posY = referenceY + orientation * index * difference / 2 * sin(frameCount / frame_rate);
+    if (index == 6) {
+      referenceX = posX + (diameter / 2 + difference) * cos(frameCount / frame_rate);
+      referenceY = posY + (diameter / 2 + difference) * sin(frameCount / frame_rate);
+    }
+    circle(posX, posY, diameter);
+  }
+}
